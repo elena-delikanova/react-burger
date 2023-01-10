531ef6677b3b55e109c933112e8eb3ddcd7e6fde
Elena Delikanova <yvdelik2@mts.ru>
2023-01-10 19:48:48 +0300

    [feat] попап с заказом

--- src/components/app-body/app-body.jsx
+++ src/components/app-body/app-body.jsx
@@ -2,17 +2,25 @@ import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import Modal from '../modal/modal';
 import BurgerIngredientCard from '../burger-ingredient-card/burger-ingredient-card';
+import OrderDetails from '../order-details/order-details';
 import { ingredients } from '../../utils/data';
 import appBodyStyles from './app-body.module.css';
 
 const AppBody = () => {
-    const selectedIngredient = ingredients[0];
+    // const selectedIngredient = ingredients[0];
+    const selectedIngredient = null;
+    const orderId = '034536';
     return (
         <main className={`${appBodyStyles.appBody} pr-5 pl-5`}>
             <BurgerIngredients />
             <BurgerConstructor />
-            <Modal header="Детали ингредиента">
-                <BurgerIngredientCard ingredient={selectedIngredient}/>
+            {selectedIngredient && (
+                <Modal header="Детали ингредиента">
+                    <BurgerIngredientCard ingredient={selectedIngredient} />
+                </Modal>
+            )}
+            <Modal>
+                <OrderDetails orderId={orderId}/>
             </Modal>
         </main>
     );
---
+++ src/components/order-details/order-details.jsx
@@ -0,0 +1,20 @@
+import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import OrderDetailsStyles from './order-details.module.css';
+
+const OrderDetails = ({orderId}) => {
+    return (
+        <div className={`${OrderDetailsStyles['order-details']} pt-4 pb-15`}>
+            <div className={`${OrderDetailsStyles['order-details__id-container']} pb-8`}>
+                <p className={`text text_type_digits-large`}>{orderId}</p>
+            </div>
+            <p className={`${OrderDetailsStyles['order-details__id-title']} text text_type_main-default pb-15`}>Идентификатор заказа</p>
+            <div className={`${OrderDetailsStyles['order-details__check-mark-container']}`}>
+                <CheckMarkIcon />
+            </div>
+            <p className={`text text_type_main-small pt-15 pb-2`}>Ваш заказ начали готовить</p>
+            <p className={`text text_type_main-small text_color_inactive`}>Дождитесь готовности на орбитальной станции</p>
+        </div>
+    );
+}
+
+export default OrderDetails;
