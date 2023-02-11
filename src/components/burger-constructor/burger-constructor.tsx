3fcbd2170a06e962e77b6a0b7ce040242313fdcf
Elena Delikanova <yvdelik2@mts.ru>
2023-02-11 19:26:31 +0300

    [feat] подключение classnames, оптимизация BurgerConstructor

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -3,13 +3,14 @@ import { useAppSelector } from '../../services/store';
 import styles from './burger-constructor.module.css';
 import Modal from '../modal/modal';
 import OrderDetails from '../order-details/order-details';
-import { ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import TotalPrice from '../total-price/total-price';
 import { addIngredient, setOrder } from '../../services/reducers/burger';
 import { resetOrderDetails } from '../../services/reducers/burger';
 import { BUN_TYPE } from '../../utils/constants';
 import { useDrop } from 'react-dnd';
 import BurgerConstructorElement from './burger-constructor-element/burger-constructor-element';
+import cs from 'classnames';
 
 const BurgerConstructor = () => {
     const dispatch = useAppDispatch();
@@ -24,13 +25,13 @@ const BurgerConstructor = () => {
         addedIngredients: ingredient[];
     } = useAppSelector((state) => state.burger);
 
-    const [, dropTarget] = useDrop({
+    const [{ isDragOver }, dropTarget] = useDrop({
         accept: 'ingredient',
         drop(ingredient: ingredient) {
             dispatch(addIngredient(ingredient));
         },
         collect: (monitor) => ({
-            isHover: monitor.isOver(),
+            isDragOver: monitor.isOver(),
         }),
     });
 
@@ -52,8 +53,14 @@ const BurgerConstructor = () => {
     });
 
     return (
-        <section className={`${styles['burger-constructor']} pt-25 pb-10 pl-10`}>
-            <section className={`${styles['burger-constructor__list']} pb-10`} ref={dropTarget}>
+        <section className={cs(styles['burger-constructor'], 'pt-25 pb-10 pl-10')}>
+            <section
+                className={cs(styles['burger-constructor__list'], {
+                    [styles['burger-constructor__list_isEmpty']]: addedIngredients.length === 0,
+                    [styles['burger-constructor__list_isDragHover']]: isDragOver,
+                })}
+                ref={dropTarget}
+            >
                 {bun && (
                     <BurgerConstructorElement
                         ingredient={bun}
@@ -63,11 +70,11 @@ const BurgerConstructor = () => {
                         type="top"
                     />
                 )}
-                <ul className={`${styles['burger-constructor__fillings']}`}>
+                <ul className={cs(styles['burger-constructor__fillings'])}>
                     {otherIngredients.map((ingredient, index) => {
                         return (
                             <li
-                                className={`${styles['burger-constructor__filling']} ${index === 0 ? '' : 'pt-4'} pr-2`}
+                                className={cs(styles['burger-constructor__filling'], index === 0 ? '' : 'pt-4', 'pr-2')}
                                 key={ingredient.uniqueId}
                             >
                                 <BurgerConstructorElement ingredient={ingredient} extraClass="ml-2" />
@@ -86,7 +93,7 @@ const BurgerConstructor = () => {
                     />
                 )}
             </section>
-            <section className={`${styles['burger-constructor__total']} pr-4`}>
+            <section className={cs(styles['burger-constructor__total'], 'pr-4 pt-10')}>
                 <TotalPrice />
                 <Button htmlType="button" type="primary" size="large" onClick={sendOrderHandler}>
                     Оформить заказ
