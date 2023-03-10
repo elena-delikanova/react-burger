68c3a1220602d50cf83cf82b26cc45b43f325d02
Elena Delikanova <yvdelik2@mts.ru>
2023-03-11 01:12:03 +0300

    [feat]  вынос формы как отдельного компонента

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,14 +1,97 @@
-import { Form } from 'formik';
-import React, { FC } from 'react';
-import styles from './form.module.css';
+import { Formik as FormikForm, FormikProps } from 'formik';
+import React from 'react';
+import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Link } from 'react-router-dom';
 import cs from 'classnames';
 
-const FormikForm: FC<{ children: React.ReactNode }> = ({ children, ...props }) => {
+import styles from './form.module.css';
+
+type FormValues = {
+    email: string;
+    password: string;
+};
+
+type InputType = {
+    type: 'text' | 'email' | 'password';
+    name: string;
+    placeholder: string;
+};
+
+type AdditionalAction = {
+    text: string;
+    linkText: string;
+    link: string;
+};
+
+const Form: React.FC<{
+    header?: string;
+    initialValues: FormValues;
+    inputs: InputType[];
+    additionalActions?: AdditionalAction[];
+}> = ({ header, initialValues, inputs, additionalActions }) => {
+    const handleSubmit = () => {
+        console.log('Тык');
+    };
     return (
-        <Form className={cs(styles.form)} {...props}>
-            {children}
-        </Form>
+        <div className={cs(styles['form__container'], 'mt-25')}>
+            {header && (
+                <div className={cs(styles['form__header-container'])}>
+                    <h2 className={cs(styles['form__header'], 'pb-6')}>{header}</h2>
+                </div>
+            )}
+
+            <FormikForm initialValues={initialValues} onSubmit={handleSubmit}>
+                {(props: FormikProps<FormValues>) => {
+                    const { handleSubmit, handleChange, handleBlur, values } = props;
+                    return (
+                        <form onSubmit={handleSubmit}>
+                            {inputs.map(({ type, name, placeholder }, index) => {
+                                return (
+                                    <Input
+                                        type={type}
+                                        placeholder={placeholder}
+                                        onChange={handleChange(name)}
+                                        onBlur={handleBlur(name)}
+                                        value={values[name as keyof FormValues]}
+                                        name={name}
+                                        extraClass="mb-6"
+                                        key={index}
+                                    />
+                                );
+                            })}
+                            <div className={cs(styles['form__button-container'])}>
+                                <Button htmlType="submit" type="primary" size="medium">
+                                    Войти
+                                </Button>
+                            </div>
+                        </form>
+                    );
+                }}
+            </FormikForm>
+            {additionalActions && (
+                <ul className={cs(styles['form__additionalActions-containers'], 'pt-20')}>
+                    {additionalActions.map(({ text, link, linkText }, index) => {
+                        return (
+                            <li
+                                className={cs(
+                                    styles['form__additionalActions-container'],
+                                    index !== additionalActions.length - 1 ? 'pb-4' : '',
+                                )}
+                                key={index}
+                            >
+                                <span className={cs('pr-2 text text_type_main-default text_color_inactive')}>
+                                    {text}
+                                </span>
+                                <Link to={link} className={cs(styles['form__additionalActions-link'])}>
+                                    {linkText}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
+        </div>
     );
 };
 
-export default FormikForm;
+export default Form;
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,53 +1,22 @@
-import { Formik } from 'formik';
-import React from 'react';
-import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
-import cs from 'classnames';
-import { Link } from 'react-router-dom';
-import styles from './login.module.css';
+import Form from '../../components/form/form';
+
+type InputType = {
+    type: 'text' | 'email' | 'password';
+    name: string;
+    placeholder: string;
+};
 
 const LoginPage = () => {
-    return (
-        <div className={cs(styles['form__container'], 'mt-25')}>
-            <div className={cs(styles['form__header-container'])}>
-                <h2 className={cs(styles['form__header'], 'pb-6')}>Вход</h2>
-            </div>
-
-            <Formik initialValues={{ email: '', password: '' }} onSubmit={() => {}}>
-                {({ handleSubmit, handleChange, handleBlur, values }) => (
-                    <form onSubmit={handleSubmit} className={cs(styles['form'])}>
-                        <Input
-                            type="email"
-                            placeholder="E-mail"
-                            onChange={handleChange('email')}
-                            onBlur={handleBlur('email')}
-                            value={values.email}
-                            name="email"
-                            extraClass="mb-6"
-                        />
-                        <Input
-                            type="password"
-                            placeholder="Пароль"
-                            onChange={handleChange('password')}
-                            onBlur={handleBlur('password')}
-                            value={values.password}
-                            name="password"
-                            extraClass="mb-6"
-                            icon="ShowIcon"
-                        />
-                        <div className={cs(styles['form__button-container'])}>
-                            <Button htmlType="submit" type="primary" size="medium">
-                                Войти
-                            </Button>
-                        </div>
-                    </form>
-                )}
-            </Formik>
-            <div className={cs(styles['form__additionalActions-container'])}>
-                <span>Вы — новый пользователь?</span>
-                <Link to={'/'}>Зарегистрироваться</Link>
-            </div>
-        </div>
-    );
+    const initialValues = { email: '', password: '' };
+    const inputs: InputType[] = [
+        { type: 'email', placeholder: 'E-mail', name: 'email' },
+        { type: 'password', placeholder: 'Пароль', name: 'password' },
+    ];
+    const additionalActions = [
+        { text: 'Вы — новый пользователь?', linkText: 'Зарегистрироваться', link: '/registration' },
+        { text: 'Забыли пароль?', linkText: 'Восстановить пароль', link: '/forgot-password' },
+    ];
+    return <Form header="Вход" initialValues={initialValues} inputs={inputs} additionalActions={additionalActions}/>;
 };
 
 export default LoginPage;
